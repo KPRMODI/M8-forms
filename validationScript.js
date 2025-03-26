10de8dac80a47acfd18c18656bda4685498deee5
KPRMODI <kpmodi@my.waketech.edu>
2025-03-26 13:16:34 -0400

    added

--- validationScript.js
+++ validationScript.js
@@ -1,27 +1,12 @@
-    // JavaScript code for form validation
-	// Prevent form from submitting
-
-      // Retrieve the input field value
-
-      // Regular expression pattern for alphanumeric input
-
-      // Check if the input value matches the pattern
-
-        // Valid input: display confirmation and submit the form
-
-        // Invalid input: display error message
-
-
-
 let inputvalue = document.getElementById("inputField")
-
-
 let button = document.getElementById("button")
 
 button.addEventListener("click", function(){
-  if (/^[a-zA-Z0-9]+$/.test(inputvalue.value)){
-    alert("lurkers io ")
+  if (/^[a-zA-Z0-9]+$/.test(inputvalue.value)){ // checks if the input is alphanumeric
+    alert("Confirmed! Your form has been submitted. Thank you")
+    inputvalue.setCustomValidity("")
   } else{
-    alert("Error you are wrong dumb ahh")
+    inputvalue.required
+    inputvalue.setCustomValidity("Error! Your input is not alphanumeric! Form not submitted.")
   }
 })
