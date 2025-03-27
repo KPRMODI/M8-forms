52be2aa4aea6fcb1a06dc43b19bddd6f557281eb
KPRMODI <kpmodi@my.waketech.edu>
2025-03-27 12:52:37 -0400

    added

--- validationScript.js
+++ validationScript.js
@@ -3,10 +3,9 @@ let button = document.getElementById("button")
 
 button.addEventListener("click", function(){
   if (/^[a-zA-Z0-9]+$/.test(inputvalue.value)){ // checks if the input is alphanumeric
-    alert("Confirmed! Your form has been submitted. Thank you")
-    inputvalue.setCustomValidity("")
+    inputvalue.setCustomValidity("Confirmed! Your form has been submitted. Thank you")
   } else{
     inputvalue.required
-    inputvalue.setCustomValidity("Error! Your input is not alphanumeric! Form not submitted.")
+    inputvalue.setCustomValidity("Error! Your input is not alphanumeric! Form not submitted try again.")
   }
 })
